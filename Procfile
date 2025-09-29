e548369ebe80c27d9c08fdff785550b0fcc2cc90
suresh <suresh68004@gmail.com>
2025-09-29 10:56:18 +0530

    build fix

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: cd tyreManagement-backend && npm start
---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Navigate to the actual project directory
+cd tyreManagement-backend
+
+# Install dependencies
+npm install
+
+# Start the application
+npm start
